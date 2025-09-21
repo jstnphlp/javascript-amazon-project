f3dcaf4efa7b5b8fbf4eb494822517f05259a0a7
jstnphlp <justinphilipmartinez@gmail.com>
2025-09-21 08:57:56 +0800

    added cart.js, used id instead of productname

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -21,7 +21,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-price">
-            $${(product.priceInCents / 100).toFixed(2)}
+            $${(product.priceCents / 100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
@@ -46,10 +46,34 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart-button"
+          data-product-id = "${product.id}">
             Add to Cart
           </button>
         </div>`;
 });
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+document.querySelectorAll('.js-add-to-cart-button').forEach((button) => {
+    button.addEventListener('click', () => {
+        const productId  = button.dataset.productId;
+        let matchingItem;
+
+        cart.forEach((item) => {
+            if(productId === item.productId) {
+                matchingItem = item;
+            }
+        });
+
+        if(matchingItem) {
+            matchingItem.quantity++;
+        } else {
+            cart.push({
+            productId,
+            quantity: 1
+        });
+        }
+
+        console.log(cart);
+    })
+})

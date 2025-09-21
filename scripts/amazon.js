3e07bde3e97963d520324fa751a6658ef93ea1c4
jstnphlp <justinphilipmartinez@gmail.com>
2025-09-21 09:18:10 +0800

    added quantity setting

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class = "js-quantity-selector-${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -60,6 +60,9 @@ document.querySelectorAll('.js-add-to-cart-button').forEach((button) => {
         const productId  = button.dataset.productId;
         let matchingItem;
 
+        let selectedQuantity = document.querySelector(`.js-quantity-selector-${productId}`);
+        let numSelectedQuantity = Number(selectedQuantity.value);
+
         cart.forEach((item) => {
             if(productId === item.productId) {
                 matchingItem = item;
@@ -71,7 +74,7 @@ document.querySelectorAll('.js-add-to-cart-button').forEach((button) => {
         } else {
             cart.push({
             productId,
-            quantity: 1
+            quantity: numSelectedQuantity
         });
         }
 

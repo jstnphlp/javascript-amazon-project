1dd32b0f1f3d844c0023c0e585ab1c3cc01ae408
jstnphlp <justinphilipmartinez@gmail.com>
2025-09-22 11:59:00 +0800

    added functionality for 'delete' in checkout

--- data/cart.js
+++ data/cart.js
@@ -1,6 +1,6 @@
-export const cart = [{
+export let cart = [{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 1
+    quantity: 2
 },
 {
     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
@@ -31,5 +31,16 @@ export function addToCart(productId) {
             quantity: numSelectedQuantity
         });
         }
+}
 
+export function removeFromCart(productId) {
+    const newCart = [];
+
+    cart.forEach((cartItem) => {
+        if(cartItem.productId !== productId) {
+            newCart.push(cartItem);
+        }
+    });
+
+    cart = newCart;
 }

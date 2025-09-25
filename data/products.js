83646b12d700ae1ac99a7d6c1bfae3cd59651336
jstnphlp <justinphilipmartinez@gmail.com>
2025-09-25 08:55:36 +0800

    added new class Clothing

--- data/products.js
+++ data/products.js
@@ -34,6 +34,27 @@ class Product {
   getPrice() {
     return `$${formatCurrency(this.priceCents)}`;
   }
+
+  extraInfoHTML() {
+    return '';
+  }
+}
+
+class Clothing extends Product {
+  sizeChartLink;
+
+  constructor(productDetails) {
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+  extraInfoHTML() {
+    return `
+      <a href="${this.sizeChartLink}" target = "_blank"">
+        Size Chart
+      </a>
+    `
+  }
 }
 
 export const products = [
@@ -139,7 +160,8 @@ export const products = [
     keywords: [
       "hoodies",
       "sweaters",
-      "apparel"
+      "apparel",
+      "clothing"
     ]
   },
   {
@@ -725,7 +747,10 @@ export const products = [
     ]
     }
 ].map((productDetails) => {
-  return new Product(productDetails);
-});
+  if(productDetails.type === 'clothing'){
+    return new Clothing(productDetails);
+  } else {
+    return new Product(productDetails);
+  }
 
-console.log(products);
+});

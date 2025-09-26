94f05bf7be2521d5fddfcf142d94596c7a97eeb8
jstnphlp <justinphilipmartinez@gmail.com>
2025-09-26 16:46:04 +0800

    promise prac

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,10 +1,43 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 //import '../data/backend-practice.js';
 //import '../data/cart-class.js';
 
-loadProducts(() => {
-    renderPaymentSummary();
+Promise.all([
+    new Promise((resolve) => {
+    loadProducts(() => {
+        resolve('value1');
+        }); 
+    }),
+
+    new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
+    })
+
+]).then((values) => {
+    console.log(values);
     renderOrderSummary();
+    renderPaymentSummary();
 });
+
+/* new Promise((resolve) => {
+    loadProducts(() => {
+    resolve('value1');
+    }); 
+
+}).then((value) => {
+    console.log(value);
+    return new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
+    });
+
+}).then(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+}); */

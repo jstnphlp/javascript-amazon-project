59c2e6449f8c8ba2175b2cad41028465f8300378
jstnphlp <justinphilipmartinez@gmail.com>
2025-09-26 15:19:35 +0800

    Update orderSummaryTest.js

--- test-jasmine/checkout/orderSummaryTest.js
+++ test-jasmine/checkout/orderSummaryTest.js
@@ -1,7 +1,14 @@
 import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
 import {loadFromStorage} from '../../data/cart.js';
+import { loadProducts } from "../../data/products.js";
 
 describe('Test Suite: renderOrderSummary', () => {
+
+    beforeAll((done) => {
+        loadProducts();
+        done();
+    });
+
     it('Displays the cart', () => {
     document.querySelector('.js-test-container').innerHTML = `
         <div class = "js-order-summary"></div>
@@ -9,14 +16,9 @@ describe('Test Suite: renderOrderSummary', () => {
 
         spyOn(localStorage, 'getItem').and.callFake(() => {
             return JSON.stringify([{
-                productId: 'id1',
+                productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
                 quantity: 1,
                 deliveryOptionId: '1'
-            },
-            {
-                productId: 'id2',
-                quantity: 1,
-                deliveryOptionId: '2'
             }]);
         });
         
